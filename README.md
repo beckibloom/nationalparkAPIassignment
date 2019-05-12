f7e6d804eebd8c0c71564efa915e75cf34134895
Becki Blumer <rblumer13@gmail.com>
2019-05-11 20:33:53 -0500

    updated javascript files to include national parks API calls

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 #Module 11 Checkpoint 7
+
 Creating a tool to display the national parks in a given area.
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 'use strict';
 
-// put your own value below!
-const apiKey = 'AIzaSyCuAeu0TG1_bkBXs1UulxF8XBbBMYMzZk8'; 
-const searchURL = 'https://www.googleapis.com/youtube/v3/search';
+const apiKey = 'GyQFctuiTazPoE2KxV20uhrUhKuTkDzmFJA3dZje'; 
+const searchURL = 'https://developer.nps.gov/api/v1/parks';
 
 
 function formatQueryParams(params) {
@@ -12,32 +11,21 @@ function formatQueryParams(params) {
 }
 
 function displayResults(responseJson) {
-  // if there are previous results, remove them
   console.log(responseJson);
   $('#results-list').empty();
-  // iterate through the items array
-  for (let i = 0; i < responseJson.items.length; i++){
-    // for each video object in the items 
-    //array, add a list item to the results 
-    //list with the video title, description,
-    //and thumbnail
+  for (let i = 0; i < responseJson.data.length; i++){
     $('#results-list').append(
-      `<li><h3>${responseJson.items[i].snippet.title}</h3>
-      <p>${responseJson.items[i].snippet.description}</p>
-      <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
+      `<li><h3><a href="${responseJson.data[i].url}">${responseJson.data[i].fullName}</a></h3>
+      <p>${responseJson.data[i].description}</p>
       </li>`
     )};
-  //display the results section  
   $('#results').removeClass('hidden');
 };
 
-function getYouTubeVideos(query, maxResults=10) {
+function getParks(state, maxResults=10) {
   const params = {
-    key: apiKey,
-    q: query,
-    part: 'snippet',
-    maxResults,
-    type: 'video'
+    stateCode: state,
+    limit: maxResults
   };
   const queryString = formatQueryParams(params)
   const url = searchURL + '?' + queryString;
@@ -46,7 +34,7 @@ function getYouTubeVideos(query, maxResults=10) {
 
   const options = {
     headers: new Headers({
-      "X-Api-Key": apiKey})
+      "X-Api-key": apiKey})
   };
 
   fetch(url, options)
@@ -65,9 +53,10 @@ function getYouTubeVideos(query, maxResults=10) {
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    const searchTerm = $('#js-search-term').val();
+    const state = $('#js-state').val();
     const maxResults = $('#js-max-results').val();
-    getYouTubeVideos(searchTerm, maxResults);
+    console.log(`Form was submitted with state ${state} and max results of ${maxResults}`);
+    getParks(state, maxResults);
   });
 }
 
